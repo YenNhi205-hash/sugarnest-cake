ac57c158ee840e018672c8b6d03fe4a3e5e6ae6b
CongHau <conghau31052004@gmail.com>
2025-05-29 08:52:10 +0700

    feat: add endpoints into CategoriesController

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Categories.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Enums;
 
 namespace API.Controllers;
 
@@ -9,11 +10,58 @@ namespace API.Controllers;
 public class CategoriesController(IMediator mediator) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetAllCategories()
+    public async Task<IActionResult> GetAll()
     {
         var req = new GetAllCategoriesQuery();
         var result = await mediator.Send(req);
         return Ok(result);
     }
 
+    [HttpGet("/valid")]
+    public Task<IActionResult> GetAllValid()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("deleted")]
+    public Task<IActionResult> GetAllDeleted()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public Task<IActionResult> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("/valid/{id}")]
+    public Task<IActionResult> GetValidById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public Task<IActionResult> Create()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> Update(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{id}")]
+    public Task<IActionResult> UpdateStatus(Guid id, Status status)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    public Task<IActionResult> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
